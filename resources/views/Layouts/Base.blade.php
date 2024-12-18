d37568a3f14a64583b2bd2b8f39717db5456e16d
YunKha <khaechal11@gmail.com>
2024-12-18 20:16:44 +0800

    First Commit

--- resources/views/Layouts/Base.blade.php
+++ resources/views/Layouts/Base.blade.php
@@ -26,14 +26,7 @@
                 </div> <!--end::Container-->
             </div> <!--end::App Content-->
         </main> <!--end::App Main--> <!--begin::Footer-->
-        <footer class="app-footer"> <!--begin::To the end-->
-            <div class="float-end d-none d-sm-inline">Anything you want</div> <!--end::To the end--> <!--begin::Copyright--> <strong>
-                Copyright &copy; 2014-2024&nbsp;
-                <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
-            </strong>
-            All rights reserved.
-            <!--end::Copyright-->
-        </footer> <!--end::Footer-->
+        
     </div> <!--end::App Wrapper--> <!--begin::Script--> <!--begin::Third Party Plugin(OverlayScrollbars)-->
     @include('Layouts.Scirpts')
 </body><!--end::Body-->
--- resources/views/Layouts/Navbar.blade.php
+++ resources/views/Layouts/Navbar.blade.php
@@ -7,7 +7,7 @@
             <li class="nav-item dropdown user-menu">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                     <img src="{{ asset('asset/dist/assets/img/user2-160x160.jpg') }}" class="user-image rounded-circle shadow" alt="User Image">
-                    <span class="d-none d-md-inline">Alexander Pierce</span>
+                    <span class="d-none d-md-inline"> Limset </span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
                     <li class="user-header text-bg-primary"> <img src="{{ asset('asset/dist/assets/img/user2-160x160.jpg') }}" class="rounded-circle shadow" alt="User Image">
